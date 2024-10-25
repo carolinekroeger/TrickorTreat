2a74843a95698c75e9aadbf214dc31974371ee43
carolinekroeger <123000427+carolinekroeger@users.noreply.github.com>
2024-10-25 12:52:54 -0400

    Add files via upload

---
+++ insex.js
@@ -0,0 +1,12 @@
+
+const images = [
+    'images/skittles.jpg',
+    'images/crunch.jpg',
+    'images/kitkat.jpg',
+    'images/nerds.jpg',
+    'images/gummybears.jpg'
+];
+
+const imageElement = document.getElementById('randomImage');
+
+//I am unsure how to make the differenet images show up at random//
